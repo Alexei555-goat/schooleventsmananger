2407a6b8103292da11260e6f81839795615038f6
alexei.galaburda@student.htl-rankweil.at <alexei.galaburda@student.htl-rankweil.at>
2026-02-26 11:29:12 +0000

    Edited EventsList.EventTemplate and EventsList

--- client_code/EventsList/__init__.py
+++ client_code/EventsList/__init__.py
@@ -11,3 +11,18 @@ class EventsList(EventsListTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    query = """
+      SELECT 
+        events.title,
+        events.date,
+        events.location,
+      FROM events
+      LEFT JOIN users ON users.user_id == events.created_by
+    """
+    returnValue = anvil.server.call('query_database', query)
+    data = []
+    for v in returnValue:
+      print(v)
+      data.append({'title': v[1], 'location': v[3]})
+
+    self.events_panel.items = data
